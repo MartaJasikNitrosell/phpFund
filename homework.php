995075c3ee90bde8e738ee26560c467ce4cda54d
MartaJasikNitrosell <75306559+MartaJasikNitrosell@users.noreply.github.com>
2021-03-11 17:49:27 +0100

    email regex

--- homework.php
+++ homework.php
@@ -1,63 +1,43 @@
 <?php
 
-/*Lab: Prepared Statements
- Complete the following:
-1. Create a prepared statement script.
-2. Add a try/catch construct.
-3. Add a new customer record binding the customer parameters.*/
+$emails = ['john_ddd@yahoo.com', 'ds_12s@', '@gmail.com', 'e@com.pl', 'EARL.GREY@EE.EA', 'example_domain.com',
+           'marta.jasik@nitro-sell.net', 'marta..jasik@nitrosell.net', 'mar-_.k@nitrosell.net', 'marta@nitrosell-.net',
+           '43434@com', '-john@gmail.com', 'john.@gmail.com', 'JOHN-DOE_23@hotmail.com'];
 
-try {
-  $pdo = new PDO('mysql:host=localhost;dbname=phpcourse','vagrant','vagrant',  [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-  $statement = $pdo->prepare( 'INSERT INTO customers (firstname,lastname,credit,loyaltyCard) VALUES (:first, :last, :credit, :loyalty)');
+$invalidCounter = 0;
 
-  $fName = 'John';
-  $lName = 'Doe';
-  $credit = 3000;
-  $loyaltyCard = true;
+//for nice output formatting
+$maxLength = max(array_map('strlen', $emails));
 
-  $statement->execute([':first' => $fName, ':last' => $lName, ':credit' => $credit, ':loyalty' => $loyaltyCard]);
-
-} catch (PDOException $e){
-  echo $e->getMessage();
+/**
+ * Prefix:
+ *    First and last character - letter or number
+ *    Other characters - letter, number, '-', '_' or '.'
+ *    Length 3-40
+ *
+ * Single @ symbol
+ *
+ * Domain:
+ *    First and last character - letter or number
+ *    Other characters - letter, number or '-'
+ *    Length 2-15
+ *
+ * Single '.' character
+ *
+ * Top-level domain:
+ *    Only letters
+ *    Length 2-9
+ *
+ * No consecutive special characters allowed.
+ * All case-insensitive.
+ */
+foreach ($emails as $email) {
+  echo $email . str_repeat(" ", $maxLength-strlen($email)) . " -> ";
+  if (!preg_match('/^(?!.*[\._\-@]{2})[a-z0-9][a-z0-9_\-\.]{2,39}@[a-z0-9\-]{2,15}\.[a-z]{2,9}$/i', $email)) {
+    $invalidCounter++;
+    echo "in";
+  }
+  echo "valid" . PHP_EOL;
 }
 
-/*Lab: Stored Procedure
- Complete the following:
-1. Create a stored procedure script.
-2. Add the SQL to the database.
-3. Call the stored procedure with parameters.*/
-try {
-  $pdo = new PDO('mysql:host=localhost;dbname=phpcourse','vagrant','vagrant', [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-  $pdo->exec('DROP PROCEDURE IF EXISTS course.newCustomer');
-
-  $pdo->exec('CREATE PROCEDURE course.newCustomer(firstn varchar(20), lastn varchar(20)) 
-        BEGIN INSERT INTO customers (firstname,lastname) VALUES (firstn, lastn) END');
-
-  $statement = $pdo->prepare( 'CALL newCustomer(?, ?)' );
-  $statement->execute(['John', 'Doe']);
-
-} catch (PDOException $e){
-  echo $e->getMessage();
-}
-
-/*Lab: Transaction
- Complete the following:
-1. Create a transaction script.
-2. Execute two SQL statements.
-3. Handle any exceptions.*/
-try {
-  $pdo = new PDO('mysql:host=localhost;dbname=phpcourse','vagrant','vagrant',
-    [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-
-  $pdo->beginTransaction();
-
-  $statement = $pdo->prepare( 'INSERT INTO customers (firstname,lastname,credit,loyaltyCard) VALUES (:first, :last, :credit, :loyalty)');
-
-  $statement->execute( [':first' => 'John', ':last' => 'Doe', ':credit' => 333, ':loyalty' => true]);
-  $statement->execute( [':first' => 'Jane', ':last' => 'Doe', ':credit' => 654, ':loyalty' => false]);
-  
-  $pdo->commit();
-} catch (PDOException $e ) {
-  $pdo->rollBack();
-  echo $e->getMessage();
-}
+echo PHP_EOL . "You provided " . (count($emails) - $invalidCounter) . " valid e-mail addresses and " . $invalidCounter . " invalid ones.";
